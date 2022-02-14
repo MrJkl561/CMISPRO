1efc89d911b7bb2befcb6d55f3a165fc7d1f6c76
Jamie Long <longjamie8@gmail.com>
2022-02-14 21:31:17 +0900

    More forloops
    Also added asg4

--- src/Asg4.java
+++ src/Asg4.java
@@ -1,4 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Jamie Long
+ *
+ */
 
 public class Asg4 {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int triNum, recNum;
+		
+		Scanner input = new Scanner(System.in); // Initializing scanner
+		/*Triangle handling*/
+		System.out.println("Enter a positive integer for how big you would like the triangle to be: "); // Asking user for an integer for the size of the triangle
+		triNum = input.nextInt();
+		
+		for(int i = 1; i <= triNum;i++) { //Generating Rows
+			for(int k = 1; k <=i; k++) { //Generating Columns
+			System.out.print(k+ " ");
+			}
+			System.out.println();
+		}
+		
+		
+		/*Rectangle handling*/
+		
+		
+		System.out.println("Enter");
+		recNum = input.nextInt();
+		
+		
+		for (recNum = 1; recNum <= 9; recNum++) {
+			
+			for(int N = 1; N <=15; N++) {
+				
+				System.out.printf( "%10d", N * recNum);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+
+
+		input.close(); // Closing scanner
+		
+	}
 
 }
--- src/EX/ForLoops.java
+++ src/EX/ForLoops.java
@@ -1,4 +1,7 @@
 package EX;
+
+import java.util.Scanner;
+
 /**
  * This program makes a multiplication table
  * 
@@ -6,17 +9,119 @@ package EX;
 public class ForLoops {
 	
 	public static void main(String[] args) {
-		int rowNumber = 0, N = 0;
+//		int rowNumber = 0, N = 0;
+//		
+//		
+//		for (rowNumber = 1; rowNumber <= 20; rowNumber++) {
+//			
+//			for(N = 1; N <=15; N++) {
+//				
+//				System.out.printf( "%10d", N * rowNumber);
+//			}
+//			System.out.println();
+//		}
 		
 		
-		for (rowNumber = 1; rowNumber <= 20; rowNumber++) {
+		Scanner input = new Scanner(System.in);
+		int recNum;
+		
+		System.out.println("Enter");
+		recNum = input.nextInt();
+		
+//		for(int i = 1; i <= recNum*2-1; i++) {
+//			
+//			for(int j = 1; j<= recNum*2-1; j++) {
+//				if(j == 2 && i==2) {
+//					System.out.print(recNum - 1);
+//				}
+//				System.out.print(recNum);
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 1; i <= recNum*2-1; i++)
+//        {
+//            for (int j = 1; j<= recNum*2-1; j++)
+//            {
+//                        
+//                    System.out.print(recNum);           
+//                          
+//            }
+//            System.out.println();
+//        }
+		
+		
+		
+		
+		for (int row = 1; row < recNum*2-1; row++) {
 			
-			for(N = 1; N <=15; N++) {
+			for(int n = 1; n < recNum*2-1; n++) {
+				if((row <= recNum && n == recNum-row+1 || n == recNum+row-1)|| (row >= recNum && n == row-recNum-1 || n == 2*recNum-(row-recNum+1)))
+				System.out.print(recNum+ " ");
+				else {
+					System.out.print(" ");
+				}
 				
-				System.out.printf( "%10d", N * rowNumber);
+				
+				
 			}
-			System.out.println();
+			
+			System.out.println( );
 		}
+		
+		
+		
+		
+		
+		
+		
+//		for (int row = 1; row <= recNum*2-1; row++) {
+//			
+//			for(int n = 1; n <= recNum*2-1; n++) {
+//				if(row <= recNum )
+//					
+//				System.out.print(recNum+ " " );
+//				
+//				else {
+//					System.out.print(recNum-1);
+//				}
+//				
+//				
+//				
+//			}
+//			
+//			System.out.println( );
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		
+//		System.out.println("Enter the number of rows: ");
+//
+//		int rows = input.nextInt();
+//		for (int i = rows; i >= 1; i--) {
+//			for (int j = rows; j >= i; j--) {
+//				if (i == 1 || j == 1)
+//					System.out.print(rows);
+//				else {
+//				System.out.print(j + " ");
+//				}
+//				
+//			}
+//
+//			System.out.println();
+//		}
+		input.close();
+		
 		
 	}
 
