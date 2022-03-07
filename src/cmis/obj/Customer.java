478f9d93eb405000854367544d7fc0c9f9cfb8da
Jamie Long <longjamie8@gmail.com>
2022-03-07 22:06:57 +0900

    Class Finished all assets updated

--- src/cmis/obj/Customer.java
+++ src/cmis/obj/Customer.java
@@ -1,78 +1,50 @@
 package cmis.obj;
 
-import java.util.Arrays;
-
 public class Customer {
 
-	private int[] customerId;
-	private int[] customerSales;
-	private String[] customerNames;
+	private int customerId;
+	private int customerSales;
+	private String customerNames;
 
-	
-	
+	public Customer() {
+	}
 
-	public Customer() {}
-
-	public Customer(int[] customerId, int[] customerSales, String[] customerNames) {
+	public Customer(int customerId, int customerSales, String customerNames) {
 		this.customerId = customerId;
 		this.customerSales = customerSales;
 		this.customerNames = customerNames;
 
 	}
 
-	public int[] getCustomerId() {
+	/* Getters and Setters */
+	public int getCustomerId() {
 		return customerId;
 	}
 
-
-
-	public String[] getCustomerName() {
+	public String getCustomerName() {
 		return customerNames;
 	}
 
-
-
-	public int[] getCustomerSales() {
+	public int getCustomerSales() {
 		return customerSales;
 	}
 
-
-
-	public void printAllData() {
-		//String[] allNames = new String[numOfCustomers];
-		System.out.printf("Their are %d of Customers. Here is the list \n", customerId.length + 1);
-		for (int i = 0; i < customerNames.length; i++) {
-		
-			System.out.println("Customer id: " + customerId[i] + " Name: " + customerNames[i] + " ");
-			
-			
-			
-		}
-		
-	}
-
-
-
-	public void setCustomerId(int[] customerId) {
+	public void setCustomerId(int customerId) {
 		this.customerId = customerId;
 	}
 
-
-
-	public void setCustomerName(String[] customerNames) {
+	public void setCustomerName(String customerNames) {
 		this.customerNames = customerNames;
 	}
 
-
-
-	public void setCustomerSales(int[] customerSales) {
+	public void setCustomerSales(int customerSales) {
 		this.customerSales = customerSales;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Customer [customerId=" + Arrays.toString(customerId) + ", customerSales="
-				+ Arrays.toString(customerSales) + ", customerNames=" + Arrays.toString(customerNames)
-				+ "]";
+		return "Customer [customerId=" + customerId + ", customerSales=" + customerSales + ", customerName="
+				+ customerNames + "]";
 	}
+
 }
--- src/cmis/obj/Project.java
+++ src/cmis/obj/Project.java
@@ -1,67 +1,144 @@
 package cmis.obj;
-
-import java.util.*;
+/**
+ * @author Jamie Long
+ * 
+ * This program will ask the user from a choice of a menu to:
+ * 1 Get Id of a customer
+ * 2 Add (a) customer(s)
+ * 3 List all customers
+ * 4 Exit program
+ * 
+ */
+import java.util.Scanner;
 
 public class Project {
+	
+	static Customer[] myCus = new Customer[100]; // Creating 100 instances of Customer object
+	static int cusCounter = 0; // Using a global counter to store how many customers have been made
 
 	public static void main(String[] args) {
-		Customer myCus = new Customer();
+		menu(); // Calling menu method
+
+	}
+
+	/* Main menu for calling methods */
+	public static void menu() {
+
+		Scanner input = new Scanner(System.in);
+		int menuNum;
+		do {
+			System.out.print("Menu: \n"
+					+ "1: Load Customer Data \n2: Add New Customer \n3: Display All Customers \n4: Exit Menu \nEnter menu choice: "); // Asking user to pick a menu option
+			menuNum = input.nextInt();
+
+			/* Switching to the menu option */
+			switch (menuNum) {
+			default:
+				System.out.println("Invalid Menu Choice");
+				break;
+			case 1:
+				if (myCus[0] == null) { // Checking whether the first instance of the Customer object is null
+					System.out.println(
+							"There are no customers in database. Please add customers before using this section.");
+					break;
+				}
+				getID(); // Calling getID method
+				break;
+
+			case 2:
+
+				addCustomer(); // Calling addCustomer method
+				break;
+
+			case 3:
+				if (myCus[0] == null) {
+					System.out.println(
+							"There are no customers in database. Please add customers before using this section.");
+					break;
+				}
+				printAllCus(); // Calling printAllCus method
+				break;
+			case 4:
+				System.exit(0); // Terminating program
+				break;
+
+			}
+		} while (menuNum != 4);
+	}
+
+	/* addCustomer method to add an customer using a for loop */
+	public static void addCustomer() {
+		/* Initializing variables */
+		Scanner inputNum = new Scanner(System.in);
+		Scanner inputText = new Scanner(System.in);
+		int numOfCus;
+		int cusId = 0;
+		int numOfSales;
+		String customerName;
+
+		System.out.print("How many customers would you like to add? "); // Asking user how many customers they would
+																		// like to add
+		numOfCus = inputNum.nextInt();
+
+		int i;
+		for (i = cusCounter; i < numOfCus + cusCounter; i++) { // For loop ensuring we add as many customers as
+																// specified and also to not override current customers
+
+			String cusIDStr = "";
+			System.out.print("\nEnter customer id (EX: 12345): ");
+			cusIDStr = inputText.nextLine();
+
+			if (cusIDStr.length() == 5) { // Checking if the id is equal to 5 in length
+				cusId = Integer.parseInt(cusIDStr); // If so assign it to cusId
+			} else
+				while (cusIDStr.length() != 5) { // If the length does not equal 5 use a while loop until the length
+													// is equal to 5
+					System.out.println("Customer ID must be 5 integers long");
+					cusIDStr = inputText.nextLine();
+					cusId = Integer.parseInt(cusIDStr);
+				}
+
+			System.out.print("\nEnter Customer name: ");
+			customerName = inputText.nextLine(); // Asking for name of customer
+
+			System.out.print("\nEnter Customer total sales: ");
+			numOfSales = inputNum.nextInt(); // Asking for total sales of customer
+
+			myCus[i] = new Customer(cusId, numOfSales, customerName); // Creating a new Customer in the array of
+																		// Customers using the input the user provided
+			System.out.println("Customer Added");
+			System.out.println();
+		}
+		cusCounter = i; // Setting cusCounter to i
+
+	}
+
+	/* Getting customer id using a for loop */
+	public static void getID() {
+		int id;
 		Scanner input = new Scanner(System.in);
 
-		menu(input, myCus);
+		System.out.print("Enter Customer ID: ");
+		id = input.nextInt(); // Asking user to enter a customer ID
 
-	}
-
-	public static void menu(Scanner input, Customer myCus) {
-		System.out.println(
-				"Menu: \n" + "1: Load Customer Data \n" + "2: Add New Customer \n" + "3: Display All Customers \n");
-
-		switch (input.nextInt()) {
-		case 1:
-
-			break;
-
-		case 2:
-			addCustomer(input, myCus);
-			break;
-		case 3:
-
-			break;
-		default:
-			break;
-		}
-	}
-
-	public static void addCustomer(Scanner input, Customer myCus) {
-		int numOfCus = 0;
-		int arrySize = 0;
-		int[] arryCus ;
-		int[] numOfSales;
-		String[] customerName;
-		
-		System.out.print("How many customers would you like to add? ");
-		numOfCus = input.nextInt();
-		arryCus = new int[numOfCus];
-		numOfSales = new int[numOfCus];
-		customerName = new String[numOfCus];
-		do {
-			
-			System.out.print("Enter customer id (EX: 12345):");
-			arryCus[arrySize] = input.nextInt();
-			myCus.setCustomerId(arryCus);
-
-			System.out.print("Enter Customer name");
-			customerName[arrySize] = input.nextLine();  
-			
-			
-			// add at end of the loop
-			if(arrySize < numOfCus) {
-				arrySize++;
+		for (int i = 0; i < myCus.length; i++) {
+			if (myCus[i].getCustomerId() == id) { // Looping thru all instances of myCus to find matching ID
+				System.out.println(myCus[i].toString()); // Printing out customer info
+				break;
 			}
-			numOfCus--;
+		}
+		System.out.println();
 
-		} while (numOfCus != 0);
-		System.out.println(myCus.toString());
+	}
+
+	/* Printing out all valid instances of Customers[] */
+	public static void printAllCus() {
+
+		for (int i = 0; i < cusCounter; i++) {
+
+			System.out.println(myCus[i].toString());
+
+		}
 	}
 
 }
